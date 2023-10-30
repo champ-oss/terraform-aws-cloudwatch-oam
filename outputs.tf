80e27dbc56bf5cf795c838a8d6787214323abe38
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-10-30 14:55:53 -0500

    updating release

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "sink_id" {
   description = "ID string that AWS generated as part of the sink ARN."
-  value       = aws_oam_sink.this[0].id
+  value       = var.enable_cw_oam_sink ? aws_oam_sink.this[0].id : null
 }
